403c973ca29a4c4f14aef7535256be3b50434d3b
derobpe <derobpe@upvnet.upv.es>
2022-02-03 12:27:22 +0100

    react navigation system

---
+++ app/screens/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Favorites(props) {
+  return (
+    <View>
+      <Text>Favorites....</Text>
+    </View>
+  );
+}
