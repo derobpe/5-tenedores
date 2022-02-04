16596c18fc5712f9dccf89171fc3d116b3f4e555
derobpe <derobpe@upvnet.upv.es>
2022-02-04 09:38:03 +0100

    Componente de Loading

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -5,9 +5,20 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
+import UserGuest from "./UserGuest";
+import UserLogged from "./UserLogged";
+
 export default function Account(props) {
+  const [login, setLogin] = useState(null);
 
-  if (login===null) return <Text>Cargando...</Text>;
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      !user ? setLogin(false) : setLogin(true);
+    });
+  }, []);
 
-  return login ? <UserLogged/> : <UserGuest/>
+  if (login === null) return <Text>Cargando...</Text>;
+
+  return login ? <UserLogged /> : <UserGuest />;
 }
