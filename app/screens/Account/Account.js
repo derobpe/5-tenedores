a628e81cd0cdf896403af9cedb57da29ee93c699
derobpe <derobpe@upvnet.upv.es>
2022-02-04 09:48:27 +0100

    añadiendo el loging a la screen account

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
 
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
@@ -7,6 +6,7 @@ import "firebase/compat/firestore";
 
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
+import Loading from "../../components/Loading";
 
 export default function Account(props) {
   const [login, setLogin] = useState(null);
@@ -18,7 +18,7 @@ export default function Account(props) {
     });
   }, []);
 
-  if (login === null) return <Text>Cargando...</Text>;
+  if (login === null) return <Loading isVisible={true} text="Cargando..."/>;
 
   return login ? <UserLogged /> : <UserGuest />;
 }
